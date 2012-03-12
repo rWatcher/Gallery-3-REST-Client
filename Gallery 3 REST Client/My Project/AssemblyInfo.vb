fccb5cd042f7c27db6dc2d683cf3cf6d9f5ac805
unknown <Eric@Sanctuary.(none)>
2012-03-12 15:18:11 -0400

    Updated version number.

--- Gallery 3 REST Client/My Project/AssemblyInfo.vb
+++ Gallery 3 REST Client/My Project/AssemblyInfo.vb
@@ -6,12 +6,12 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("A desktop client for connecting to Gallery 3.")> 
 <Assembly: AssemblyCompany("rWatcher")> 
 <Assembly: AssemblyProduct("Gallery 3 REST Client")> 
-<Assembly: AssemblyCopyright("Copyright 2010 Eric Cavaliere")> 
+<Assembly: AssemblyCopyright("Copyright 2010-2012 Eric Cavaliere")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
 
 <Assembly: Guid("dd5eb119-bde6-4114-b2aa-3c7a24eee8cc")> 
 
-<Assembly: AssemblyVersion("1.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0")> 
+<Assembly: AssemblyVersion("1.2.0")> 
+<Assembly: AssemblyFileVersion("1.2.0")> 
